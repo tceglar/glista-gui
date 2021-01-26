36b9db785afb7d8df7aaaf3761429f1eef7ac777
tceglar <tilen.ceglar@gmail.com>
2021-01-26 15:59:22 +0100

    add deploy script in package.json

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
   </div>
 </template>
 
-<scripts lang="perts">
+<script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
