f0aed63fc19f3f81706f6bfba5045e7a79c7959d
tceglar <tilen.ceglar@gmail.com>
2021-01-31 18:48:16 +0100

    gh-pages

---
+++ precache-manifest.eb64bde2ce53e8994e7d1f2e829191d8.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c033483e4c1add66acb",
+    "url": "/glista-gui/css/app.bf716a94.css"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/glista-gui/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/glista-gui/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/glista-gui/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/glista-gui/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "a794a15f6e45b941d8a521e7965221a8",
+    "url": "/glista-gui/img/brand/favicon.png"
+  },
+  {
+    "revision": "59d7c2b119ab46fc7b5f853f04324c89",
+    "url": "/glista-gui/img/brand/green.png"
+  },
+  {
+    "revision": "76d68da6df1dd2ce6356207ca3e53058",
+    "url": "/glista-gui/img/brand/pickle_rick.png"
+  },
+  {
+    "revision": "2e3d71f8f1960d0edfcd8095328119f7",
+    "url": "/glista-gui/img/brand/pickle_rick_.png"
+  },
+  {
+    "revision": "e0ea3f1916671872498288dc2bd40f06",
+    "url": "/glista-gui/img/brand/white.png"
+  },
+  {
+    "revision": "46abbc4a676739dbd61f8a305cb63fd8",
+    "url": "/glista-gui/img/nucleo-icons.46abbc4a.svg"
+  },
+  {
+    "revision": "1f533d2fdc40493ee834dd8e4288220c",
+    "url": "/glista-gui/img/theme/angular.jpg"
+  },
+  {
+    "revision": "d136a071cf09ba5ab8dc2ada1ab04015",
+    "url": "/glista-gui/img/theme/bootstrap.jpg"
+  },
+  {
+    "revision": "cee60cfd101f9b3d2aae0e819ec8b267",
+    "url": "/glista-gui/img/theme/profile-cover.jpg"
+  },
+  {
+    "revision": "50dd5fdc839dfc16739b15370650485c",
+    "url": "/glista-gui/img/theme/react.jpg"
+  },
+  {
+    "revision": "3b8c576082e5ca27bae21a839f1290f6",
+    "url": "/glista-gui/img/theme/sketch.jpg"
+  },
+  {
+    "revision": "edc7106b21ec12e57022b2ebd534cd2d",
+    "url": "/glista-gui/img/theme/team-1-800x800.jpg"
+  },
+  {
+    "revision": "be997d5226b992ffad34816870c6b7aa",
+    "url": "/glista-gui/img/theme/team-2-800x800.jpg"
+  },
+  {
+    "revision": "54e3f3c414bd8e7234bae3ee3be950e5",
+    "url": "/glista-gui/img/theme/team-3-800x800.jpg"
+  },
+  {
+    "revision": "66618a418175ddf2ac8c47a241d327a8",
+    "url": "/glista-gui/img/theme/team-4-800x800.jpg"
+  },
+  {
+    "revision": "c532f6f0176968caa6265864ee1abb35",
+    "url": "/glista-gui/img/theme/vue.jpg"
+  },
+  {
+    "revision": "e8c613be05efc1910c256ba94e254629",
+    "url": "/glista-gui/index.html"
+  },
+  {
+    "revision": "1c033483e4c1add66acb",
+    "url": "/glista-gui/js/app.8889798d.js"
+  },
+  {
+    "revision": "774c3f94a543505bddbf",
+    "url": "/glista-gui/js/chunk-vendors.72b78205.js"
+  },
+  {
+    "revision": "6ee11ee140f6300f0daa",
+    "url": "/glista-gui/js/demo.44bc0aab.js"
+  },
+  {
+    "revision": "2c6190eb814d2dd4d454ae170c43c7c2",
+    "url": "/glista-gui/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/glista-gui/robots.txt"
+  }
+]);
